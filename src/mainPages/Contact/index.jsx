c481427441d72df0142481487b9693a733d1b9a3
V.VINCLIN JENU <vinclinjenuv@gmail.com>
2025-01-23 16:44:53 +0530

    map update

--- src/mainPages/Contact/index.jsx
+++ src/mainPages/Contact/index.jsx
@@ -23,20 +23,20 @@ export default function Contact() {
               <span>
                 <LocationOnIcon />
               </span>
-              156 Sixth Avenue, Nashik Nagar, NN 20990
+              10-L, Duraisamy Naidu St, Dharmapuri Bazaar, Dharmapuri - 636701
             </p>
             <p>
               {" "}
               <span>
                 <EmailTwoToneIcon />
               </span>{" "}
-              shringaarcontact@gmail.com
+              @gmail.com
             </p>
             <p>
               <span>
                 <LocalPhoneRoundedIcon />
               </span>
-              +91 959595959
+              +91 9442252929
             </p>
           </p>
         </div>
@@ -88,7 +88,7 @@ export default function Contact() {
       </div>
 
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d170797.8327653595!2d73.65696696641196!3d19.921946582207333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sshringaar%20nshik!5e0!3m2!1sen!2sin!4v1685948098610!5m2!1sen!2sin"
+         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.690347015381!2d78.16195952482666!3d12.13332658811027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bac1710b63ce999%3A0x10b6fbe35bdaf865!2sDn%20dhanapal%20jewellers!5e0!3m2!1sen!2sin!4v1737627843807!5m2!1sen!2sin"
         width="100%"
         height="450"
         allowfullscreen=""
