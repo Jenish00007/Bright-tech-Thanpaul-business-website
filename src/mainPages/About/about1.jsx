71e8f4f0effd6363a67383402dbed77570a29549
Fenix <fenixt.s2000@gmail.com>
2025-01-28 19:53:20 +0530

    About changes

--- src/mainPages/About/about1.jsx
+++ src/mainPages/About/about1.jsx
@@ -7,27 +7,31 @@ export default function AboutFounder() {
     <section className="aboutFounder">
       <img src="\assets\shop1.png" alt="Founder Photo" width="300px" />
       <div className="textContent">
-        <h5>Dhanapal Jewellers</h5>
         <h3>About the Founder</h3>
-        <h4>
+        <h5>Dhanapal Jewellers</h5>
+        {/* <h4>
         NK Dhanapal Jewellers is a jewelry store located at 10-L, Duraisamy Naidu Street, Dharmapuri, Tamil Nadu 636701, India. 
-        </h4>
+        </h4> */}
         <p>
-        For a broader selection of jewelry options in Dharmapuri, other notable jewelers include Sahana Jewellers, Malabar Gold and Diamond Jewellers, STS Jewellery, Tanishq, and Sivaraj.         </p>
+        NK Dhanapal Jewellers is a jewelry store located at 10-L, Duraisamy Naidu Street, Dharmapuri, Tamil Nadu 636701, India. 
+        For a broader selection of jewelry options in Dharmapuri, other notable jewelers include Sahana Jewellers, Malabar Gold and Diamond Jewellers, STS Jewellery, Tanishq, and Sivaraj.         
+        </p>
         
       </div>
     </section>
     <section className="aboutFounder">
       
       <div className="textContent">
-        <h5>Dhanapal Jewellers</h5>
+     
         <h3>About the Founder</h3>
-        <h4>
+        <h5>Dhanapal Jewellers</h5>
+        {/* <h4>
         NK Dhanapal Jewellers is a jewelry store located at 10-L, Duraisamy Naidu Street, Dharmapuri, Tamil Nadu 636701, India. 
-        </h4>
+        </h4> */}
         <p>
-        For a broader selection of jewelry options in Dharmapuri, other notable jewelers include Sahana Jewellers, Malabar Gold and Diamond Jewellers, STS Jewellery, Tanishq, and Sivaraj.         </p>
-        
+        NK Dhanapal Jewellers is a jewelry store located at 10-L, Duraisamy Naidu Street, Dharmapuri, Tamil Nadu 636701, India. 
+        For a broader selection of jewelry options in Dharmapuri, other notable jewelers include Sahana Jewellers, Malabar Gold and Diamond Jewellers, STS Jewellery, Tanishq, and Sivaraj.         
+        </p>
       </div>
       <img src="\assets\shop.png" alt="Founder Photo" width="300px" />
     </section>
